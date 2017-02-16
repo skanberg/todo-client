e6ec47442d08c587aab287f639b9710a887900b8
Fredrik Skanberg <skanberg@gmail.com>
2017-02-16 20:13:54 +0100

    Added Material Design Lite.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById("root")
 );
