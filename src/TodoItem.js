aa82c9210b5883245d86f065d569182d489a180c
Fredrik Skanberg <skanberg@gmail.com>
2017-02-18 18:47:43 +0100

    Added icon buttons.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { ListItem, ListItemContent, ListItemAction, Icon } from "react-mdl";
+import {
+  ListItem,
+  ListItemContent,
+  ListItemAction,
+  IconButton
+} from "react-mdl";
 
 const TodoItem = ({ id, name, description, completed }) => (
   <ListItem twoLine>
     <ListItemContent avatar="priority_high" subtitle={description}>{ name }</ListItemContent>
     <ListItemAction>
-      <a href="#"><Icon name="star" /></a>
+      <IconButton ripple name="delete"/>
+      <IconButton ripple name="panorama_fish_eye"/>
+      <IconButton colored ripple name="check_circle"/>
     </ListItemAction>
   </ListItem>
 );
